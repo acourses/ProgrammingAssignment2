87a92b34d658a13cd709ef82e98a49f4a281eb41
acourses <acourses@yandex.com>
2014-08-23 23:39:06 +0400

    Cached inverse matrix computation implemented

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inverse computation cache module.
 
-## Write a short comment describing this function
+## **makeCacheMatrix**
+## makeCacheMatrix function wraps a matrix into an object that can store
+## two matrixes: original matrix and its cached inverse.
+## The result object itself is a list of named functions that share same parent evironment.
+## Original matrix can be accessed with get() and set() functions.
+## Cached inverse can be accessed with getInverse() and setInverse() functions.
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  # define variables to store matrix and its inverse in makeCacheMatrix environment
+  
+  value <- x
+  cachedInverse <- NULL
+  
+  # define accessor functions
+  
+  setValue <- function(newX = matrix()) {
+    value <<- newX
+    cachedInverse <- NULL
+  }
 
+  getValue <- function() value
+  
+  setInverseValue <- function(newInverse = matrix()) {
+    cachedInverse <<- newInverse
+  }
+  
+  getInverseValue <- function() cachedInverse
+  
+  ## return list of named accessor functions
+  
+  list(set = setValue, get = getValue,
+       setInverse = setInverseValue, getInverse = getInverseValue)
 }
 
 
-## Write a short comment describing this function
+## **cacheSolve**
+## cacheSolve functions takes a matrix wrapped with makeCacheMatrix
+## function and returns the inverse value of the matrix.
+## Actual computation is performed only once per each wrapped matrix.
+## Consequent calls to cacheSolve on the same wrapped matrix
+## will return the cached result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if (!is.null(inverse)) {
+    ## the inverse was already stored in x
+    message("Using cached inverse value")
+    return(inverse) 
+  }
+  
+  ## the inverse wasn't stroed in x, compute
+  inverse <- solve(x$get(), ...)
+  
+  ## store computed value in x, and return it
+  x$setInverse(inverse)
+  inverse
 }
+
+
+
+
+
+
+
+
+
